9e5948536193ab270db893c8d27361f0254beb15
carloseduard.s7@gmail.com <carloseduard.s7@gmail.com>
2023-04-19 00:45:05 -0300

    Conversor d temperatura

---
+++ ConversorDeTemperatura/ConversorDeTemperatura/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+
+            Console.WriteLine("### CONVERSOR DE TEMPERATURA ####");
+            Console.Write("Insira a temperatura em celcius: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------------");
+
+
+            // (c * 9 / 5) + 32 = Fahrenheit
+
+            f = (c * 9 / 5) + 32;
+
+            // c + 275,15 = kelvin
+
+            k = c + 273.15;
+
+            Console.WriteLine(c + " graus celcius = " + f + " graus fahrenheit");
+            Console.WriteLine(c +" "+ "graus celcius = " + k + "graus kelvin");
+            Console.WriteLine("-----------------------------------");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
